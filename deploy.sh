77b4c2fba213aa937747b2ba18dded1a9a4d5f39
Tai <hotai1806@gmail.com>
2021-10-05 15:17:01 +0700

    update script deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-npm run build --prefix backend/
+ssh -i ~/key-pair-app-chat.pem azureuser@20.55.61.85 "pkill node"
+ssh -i ~/key-pair-app-chat.pem azureuser@20.55.61.85 "sudo rm -rf backend/"
+ssh -i ~/key-pair-app-chat.pem azureuser@20.55.61.85 "mkdir backend/"
+
 scp -r -i ~/key-pair-app-chat.pem ./backend/dist azureuser@20.55.61.85:/home/azureuser/backend/dist
 scp -r -i ~/key-pair-app-chat.pem ./backend/tsconfig.build.json azureuser@20.55.61.85:/home/azureuser/backend
 scp -r -i ~/key-pair-app-chat.pem ./backend/package.json azureuser@20.55.61.85:/home/azureuser/backend

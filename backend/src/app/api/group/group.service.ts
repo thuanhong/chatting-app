bce50fcd88e2143bd00cdf12fe90f37f6acede70
Tai <hotai1806@gmail.com>
2021-11-03 16:19:10 +0700

    hard fix api group service

--- backend/src/app/api/group/group.service.ts
+++ backend/src/app/api/group/group.service.ts
@@ -14,40 +14,50 @@ export class GroupService {
     userId: string,
     pageInfo: PagingInfo,
   ): Promise<GroupChat[]> {
-    const allGroupId = await this.dataService.find(UserGroup, {
-      select: ['groupId', 'userId'],
-      where: {
-        userId: userId,
-      },
-    });
+    try {
+      const allGroupId = await this.dataService.find(UserGroup, {
+        select: ['groupId', 'userId'],
+        where: {
+          userId: userId,
+        },
+      });
+      if (allGroupId.length === 0) {
+        return [];
+      }
+      const listGroupId = allGroupId.map((contact) => contact.groupId);
+      if (!listGroupId || listGroupId.length === 0) {
+        return [];
+      }
+      const getUsers = await (
+        await this.dataService.asQuery(Users, 'users')
+      )
+        .leftJoinAndSelect('users.userGroups', 'userGroups')
+        .where('userGroups.group_id IN(:...listGroupId)', { listGroupId })
+        .getRawMany()
+        .then((groups) => groups.filter((group) => group.users_id !== userId));
 
-    const listGroupId = allGroupId.map((contact) => contact.groupId);
-    const getUsers = await (
-      await this.dataService.asQuery(Users, 'users')
-    )
-      .leftJoinAndSelect('users.userGroups', 'userGroups')
-      .where('userGroups.group_id IN(:...listGroupId)', { listGroupId })
-      .getRawMany()
-      .then((groups) => groups.filter((group) => group.users_id !== userId));
-
-    let listGroupChat = await this.dataService.find(GroupChat, {
-      select: ['id', 'groupName', 'lastMessage'],
-      where: {
-        id: In(listGroupId),
-      },
-      take: pageInfo.take ?? 15,
-      skip: pageInfo.pageIndex * pageInfo.take ?? 1,
-      order: {
-        modifiedAt: 'DESC',
-      },
-    });
-    let newList = listGroupChat.map((res) => {
-      let infoUser = getUsers.filter(
-        (col) => col.userGroups_group_id === res.id,
-      );
-      return { ...res, infoUser: { ...infoUser[0] } };
-    });
-    return newList;
+      let listGroupChat = await this.dataService.find(GroupChat, {
+        select: ['id', 'groupName', 'lastMessage'],
+        where: {
+          id: In(listGroupId),
+        },
+        take: pageInfo.take ?? 15,
+        skip: pageInfo.pageIndex * pageInfo.take ?? 1,
+        order: {
+          modifiedAt: 'DESC',
+        },
+      });
+      let newList = listGroupChat.map((res) => {
+        let infoUser = getUsers.filter(
+          (col) => col.userGroups_group_id === res.id,
+        );
+        return { ...res, infoUser: { ...infoUser[0] } };
+      });
+      return newList;
+    } catch (e) {
+      console.log('LIST API GROUP', e);
+      return e;
+    }
   }
 
   async updateUserGroupChat(id: string, entities: GroupDto): Promise<any> {
